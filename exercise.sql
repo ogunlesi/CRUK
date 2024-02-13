e1011c0c21b6444092d92bc7877cfa4cf1f9bd6e
ogunlesi <159722658+ogunlesi@users.noreply.github.com>
2024-02-13 18:14:12 +0000

    exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -10,11 +10,11 @@ taxi_data.head()
 
 # Select necessary columns
 selected_columns = ["trip_distance", "tpep_pickup_datetime", "tpep_dropoff_datetime", "PULocationID"]
-df = data[selected_columns]
+df = taxi_data[selected_columns]
 
 # Correct data quality issue
-df.loc[data['PULocationID'] == 161, 'PULocationID'] = 237
-df.loc[data['PULocationID'] == 237, 'PULocationID'] = 161
+df.loc[taxi_data['PULocationID'] == 161, 'PULocationID'] = 237
+df.loc[taxi_data['PULocationID'] == 237, 'PULocationID'] = 161
 
 #  updated data to a new Parquet file
 df.to_parquet("yellow_tripdata_2021-03_updatedv1.parquet")
@@ -22,6 +22,39 @@ df.to_parquet("yellow_tripdata_2021-03_updatedv1.parquet")
 #  bringing updated data into dataframe 
 taxi_data_updated = pd.read_parquet("yellow_tripdata_2021-03_updatedv1.parquet")
 
+  
+# python for average speed
+# Calculate total distance travelled in miles
+total_distance = taxi_data_updated['trip_distance'].sum()
+
+# Calculate total time taken in seconds
+total_time = (taxi_data_updated['tpep_dropoff_datetime'] - taxi_data_updated['tpep_pickup_datetime']).dt.total_seconds().sum()
+
+# Convert total time to hours
+total_time_hours = total_time / 3600
+
+# Calculate average speed in miles per hour
+average_speed = total_distance / total_time_hours
+print("Average speed in March 2021:", average_speed, "miles per hour")
+
+# Calculate speed for each trip
+taxi_data_updated['speed'] = taxi_data_updated['trip_distance'] / ((taxi_data_updated['tpep_dropoff_datetime'] - taxi_data_updated['tpep_pickup_datetime']).dt.total_seconds() / 3600)
+
+# Calculate mean speed for all trips in March 2021
+mean_speed = taxi_data_updated['speed'].mean()
+
+# Filter trips slower than the average speed
+slow_trips = taxi_data_updated[taxi_data_updated['speed'] < mean_speed]
+
+# Group by week and Pick up locations, count occurrences, and sort
+top_pickup_locations = slow_trips.groupby([pd.Grouper(key='tpep_pickup_datetime'), 'PULocationID']).size().reset_index(name='count').sort_values(by='count', ascending=False)
+
+# Extract top 5 pick-up locations for each week
+top_pickup_locations_grouped = top_pickup_locations.groupby(pd.Grouper(key='tpep_pickup_datetime')).head(5)
+
+print("Top 5 Pick-up locations for each week resulting in trips slower than the average speed:")
+print(top_pickup_locations_grouped)
+  
 SQL for average speed
 --- 60 mins in an hour & 60 seconds in a munutes 60x60 g
 SELECT AVG(trip_distance / TIMESTAMP_DIFF(tpep_dropoff_datetime, tpep_pickup_datetime, SECOND)) * 3600 AS avg_speed_mph
